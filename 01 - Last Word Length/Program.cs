297e197685f4b4cdbe878536e0d884e8c4c353f8
Kibru Genene <positivevibes.kg@gmail.com>
2025-03-10 22:34:46 +0300

    first commit

---
+++ 01 - Last Word Length/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public static int LengthOfLastWord(string s) {
+        int l=0;
+        int i=s.Length-1;
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            l++;
+            i--;
+        }
+        return l;
+    }
+
+    public static void Main () {
+        string s = "  we are  flying to the  moon   ";
+        Console.Write(LengthOfLastWord (s));
+    }
+}
+
---
+++ 02 - Single Number/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+        // Initialize the result to 0
+        // Loop through each number in the array
+        // XOR the current number with the result
+        // This cancels out numbers that appear twice
+        // and leaves the number that appears once
+        // Return the result, which is the number that appears once
+
+public class Solution {
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        
+        foreach (int num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void Main () {
+        int [] nums = {4, 1, 2, 1, 2};
+        Console.Write (SingleNumber ());
+    }
+}
